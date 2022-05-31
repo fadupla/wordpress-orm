4652c37c3c8a20e88d1ad38dc988bf3a468c465d
Fabien <fabien@superprof.com>
2022-05-31 09:11:40 +0200

    Add: creation of indexes from model

---
+++ src/Exceptions/UnknownIndexTypeException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Symlink\ORM\Exceptions;
+
+/**
+ * Class UnknownIndexTypeException
+ *
+ * @package Symlink\ORM
+ */
+class UnknownIndexTypeException extends \Exception {
+}
--- src/Mapping.php
+++ src/Mapping.php
@@ -25,6 +25,16 @@ class Mapping {
 	private $models;
 
 	/**
+	 * @type string[]
+	 */
+	private $model_property_index_types = [
+		'index',
+		'unique',
+		'spatial',
+		'fulltext',
+	];
+
+	/**
 	 * Initializes a non static copy of itself when called. Subsequent calls
 	 * return the same object (fake dependency injection/service).
 	 *
@@ -152,7 +162,7 @@ class Mapping {
 						'float',
 					] )
 					) {
-						throw new \Symlink\ORM\Exceptions\UnknownColumnTypeException( sprintf( __( 'Unknown model property column type %s set in @ORM_Column_Type on model %s..' ), $column_type, $classname ) );
+						throw new \Symlink\ORM\Exceptions\UnknownColumnTypeException( sprintf( __( 'Unknown model property column type "%s" set in @ORM_Column_Type on model %s..' ), $column_type, $classname ) );
 					}
 
 					// Build the rest of the schema partial.
@@ -186,6 +196,41 @@ class Mapping {
 					$this->models[ $classname ]['schema'][ $property->name ]      = $schema_string;
 					$this->models[ $classname ]['placeholder'][ $property->name ] = $placeholder_values_type;
 				}
+
+				/**
+				 * Index
+				 */
+				if ( $property_annotation->get( 'ORM_Index_Type' ) ) {
+
+					$index_type = $property_annotation->get( 'ORM_Index_Type' );
+
+					// Test the ORM_Index_Type
+					if ( ! in_array( $index_type, $this->model_property_index_types ) ) {
+						throw new \Symlink\ORM\Exceptions\UnknownIndexTypeException( sprintf( __( 'Unknown model property index type "%s" set in @ORM_Column_Type on model %s. It should be one of: "%s".' ), $index_type, $classname, implode( '","', $this->model_property_index_types ) ) );
+					}
+
+					// Index column(s)
+					if ( $property_annotation->get( 'ORM_Index_Columns' ) ) {
+						$columns_string = $property_annotation->get( 'ORM_Index_Columns' );
+					} else {
+						$columns_string = $property->name;
+					}
+
+					// Index name
+					if ( $property_annotation->get( 'ORM_Index_Name' ) ) {
+						$index_name = $property_annotation->get( 'ORM_Index_Name' );
+					} else {
+						// Build an index name with index column(s)
+						$build_index_name = str_replace( ' ', '', trim( $columns_string ) );
+						$build_index_name = str_replace( ',', '_', $build_index_name );
+						$index_name       = $build_index_name;
+					}
+
+					$index_string = sprintf( "ADD %s `%s` (%s)", $index_type, $index_name, $columns_string );
+
+					$this->models[ $classname ]['index'][ $property->name ] = $index_string;
+				}
+
 			}
 
 
@@ -217,7 +262,7 @@ class Mapping {
 
 		// Are we allowed to update the schema of this model in the db?
 		if ( ! $mapped['ORM_AllowSchemaUpdate'] ) {
-			throw new \Symlink\ORM\Exceptions\AllowSchemaUpdateIsFalseException( sprintf( __( 'Refused to update model schema %s. ORM_AllowSchemaUpdate is FALSE.' ), $classname ) );
+			throw new \Symlink\ORM\Exceptions\AllowSchemaUpdateIsFalseException( sprintf( __( 'Cannot update model schema %s. ORM_AllowSchemaUpdate is set to FALSE.' ), $classname ) );
 		}
 
 		// Create an ID type string.
@@ -236,6 +281,72 @@ class Mapping {
 		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 
 		return dbDelta( $sql );
+	}
+
+
+	/**
+	 * Add indexes of model. Indexes can be reset on an update for example
+	 *
+	 * @param string $classname
+	 * @param bool $reset_indexes
+	 *
+	 * @return array
+	 * @throws Exceptions\AllowSchemaUpdateIsFalseException
+	 * @throws Exceptions\RepositoryClassNotDefinedException
+	 * @throws Exceptions\RequiredAnnotationMissingException
+	 * @throws Exceptions\UnknownColumnTypeException
+	 */
+	public function updateIndexes( string $classname, bool $reset_indexes ): array {
+
+		global $wpdb;
+
+		// Init
+		$messages = [];
+
+		// Get the model annotation data.
+		$mapped = $this->getProcessed( $classname );
+
+		// Get table name
+		$table_name = $wpdb->prefix . $mapped['ORM_Table'];
+
+		// Are we allowed to update the schema of this model in the db?
+		if ( ! $mapped['ORM_AllowSchemaUpdate'] ) {
+			throw new \Symlink\ORM\Exceptions\AllowSchemaUpdateIsFalseException( sprintf( __( 'Cannot update model schema %s. ORM_AllowSchemaUpdate is set to FALSE.' ), $classname ) );
+		}
+
+		if ( $reset_indexes ) {
+			// Drop previous indexes
+			$sql_drops = [];
+			$results   = $wpdb->get_results( sprintf( "SHOW INDEX FROM `%s`", $table_name ) );
+			foreach ( $results as $result ) {
+				$index_name = $result->Key_name;
+
+				if ( $index_name !== 'PRIMARY' ) {
+					$sql_drops[] = sprintf( "DROP INDEX `%s`", $index_name );
+				}
+			}
+			$query_result = $wpdb->query( sprintf( "ALTER TABLE `%s` %s", $table_name, implode( ",\n", $sql_drops ) ) );
+			$messages[]   = ( $query_result ) ? __( 'Indexes were dropped' ) : __( 'Indexes could not be dropped' );
+		}
+
+		// Any indexes to add?
+		if ( isset( $mapped['index'] ) ) {
+
+			// Build the SQL ALTER TABLE command
+			$sql = sprintf( "ALTER TABLE %s %s;", $table_name, implode( ",\n", $mapped['index'] ) );
+
+			$query_result = $wpdb->query( $sql );
+			if ( $query_result ) {
+				$messages[] = sprintf( _n( '%s indexe was added to table %s', '%s indexes were added to table %s', count( $mapped['index'] ) ), count( $mapped['index'] ), $table_name );
+			} else {
+				$messages[] = __( 'Indexes could not be updated' );
+			}
+
+		} else {
+			$messages[] = __( 'No indexes to update' );
+		}
+
+		return $messages;
 	}
 
 	/**
